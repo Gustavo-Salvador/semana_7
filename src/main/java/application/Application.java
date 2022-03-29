fe9d166b6ae708de2036840de2f95915a6a32ed1
ghostracoon <gusalvador1028@gmail.com>
2022-03-29 18:36:07 +0000

    correção

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,5 +1,9 @@
 package application;
+import org.springframework.boot.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
-    
+    public static void main(String[] args) {
+        
+    }
 }
