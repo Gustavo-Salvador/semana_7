b9af04fbb4c076439a5c32f544762ecb99aad896
ghostracoon <gusalvador1028@gmail.com>
2022-03-29 19:46:23 +0000

    criando a base de dados

--- src/main/java/application/models/Livro.java
+++ src/main/java/application/models/Livro.java
@@ -1,7 +1,29 @@
 package application.models;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="livros")
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
 
 }
